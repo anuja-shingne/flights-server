30798940443885a3f49fa7786ae3e608f2e8881c
anuja <anuja.s.work@gmail.com>
2020-12-10 01:23:05 +0100

    Fixed CORS issue

--- src/main/java/com/cgi/trips/controller/FlightsController.java
+++ src/main/java/com/cgi/trips/controller/FlightsController.java
@@ -1,6 +1,7 @@
 package com.cgi.trips.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +14,13 @@ import io.swagger.annotations.ApiOperation;
 
 @RestController
 @Api(value = "APIs to peform crud operation on the itinerary")
+@CrossOrigin
 public class FlightsController {
 
 	@Autowired
 	private IFlightService flightService;
 
-	@GetMapping("/")
+	@GetMapping("/trips")
 	@ApiOperation(value = "get the itinerary of the flights", response = FlightsDTO.class)
 	public FlightDTO getItinerary() {
 		return flightService.getFlightItinerary();
